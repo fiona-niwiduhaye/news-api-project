58585711a34908a5d40421af1b6ff81963dffc10
fiona <fionaniwe@gmail.com>
2020-11-22 19:53:54 +0200

    first commit

---
+++ app/config.py
@@ -0,0 +1,31 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    NEWS_API_LINK='https://newsapi.org/v2/sources?category=general&apiKey={}'
+
+    ARTICLE_API_BASE_URL= 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    
+
+class ProdConfig:
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    pass
+
+
+class DevConfig:
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    NEWS_API_LINK='https://newsapi.org/v2/sources?category=general&apiKey={}'
+
+    DEBUG = True
---
+++ app/views.py
@@ -0,0 +1,25 @@
+from flask import render_template
+from app import app
+from .request import get_sources
+from .request import get_articles
+from .models import Source,Article
+# Views
+@app.route('/')
+def index():
+      """
+      View root page function that returns the index page and its data
+      """
+      #Getting source
+      sources = get_sources('sources')
+
+      return render_template('index.html', sources = sources)
+
+@app.route('/articles/<id>')
+def articles(id):
+    """
+    View Articles page function that returns the source details page and its data
+    """
+    articles = get_articles(id)
+    title= f'{id}'
+
+    return render_template('articles.html', id = id,articles= articles)
